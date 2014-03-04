2902176ce49c985a3b20656d5451f850f1a642e0
Руслан <kraktus@mail.ru>
2014-03-04 04:51:53 +0400

    Доделал персональную страницу медиа и страницу списка медиа

--- www/wp-content/themes/yoo_bigeasy_wp/warp/systems/wordpress/layouts/_post.php
+++ www/wp-content/themes/yoo_bigeasy_wp/warp/systems/wordpress/layouts/_post.php
@@ -7,9 +7,9 @@
 
 <?
 $post_info = get_post_custom();
-/*echo "<pre>";
-var_dump($post_info);
-echo "</pre>";*/
+// echo "<pre>";
+// var_dump($post_info);
+// echo "</pre>";
 /*ВНИМАНИЕ МНОГО ГОВНОКОДА - Я НЕ УСПЕВАЛ :(*/?>
 <?if($post_info["wpcf-city"][0] == 1)//Если город выбран правильно, то это Мск
         $post_info["wpcf-city"][0] = 'Москва';
@@ -80,6 +80,96 @@ else
 	<h4><?php edit_post_link(__('Изменить данные события', 'warp'), '<p class="edit">','</p>'); ?></h4>
 
 </article>
+<?elseif (get_post_type() === 'media'):?>
+<?
+/**
+ * ШАБЛОН ДЛЯ ПУБЛИКАЦИИ ТИПА MEDIA(медиа-отчеты)
+ */
+?>
+
+<?
+$post_info = get_post_custom();
+// echo "<pre>";
+// var_dump($post_info);
+// echo "</pre>";
+/*ВНИМАНИЕ МНОГО ГОВНОКОДА - Я НЕ УСПЕВАЛ :(*/?>
+<?if($post_info["wpcf-city"][0] == 1)//Если город выбран правильно, то это Мск
+        $post_info["wpcf-city"][0] = 'Москва';
+else
+        $post_info["wpcf-city"][0] =  $post_info["wpcf-another_city"][0];
+?>
+<article id="item-<?php the_ID(); ?>" class="item" data-permalink="<?php the_permalink(); ?>">
+
+        <header>
+
+                <h1 class="title"><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+
+                <!-- <p class="meta">
+                        <?php
+                                $date = '<time datetime="'.get_the_date('Y-m-d').'" pubdate>'.get_the_date().'</time>';
+                                printf(__('Written by %s on %s. Posted in %s', 'warp'), '<a href="'.get_author_posts_url(get_the_author_meta('ID')).'" title="'.get_the_author().'">'.get_the_author().'</a>', $date, get_the_category_list(', '));
+                        ?>
+                </p> -->
+
+        </header>
+
+<table style="width: 100%;" border="0" cellspacing="0" cellpadding="0">
+<tbody>
+<tr align="right">
+<td rowspan="2" colspan="1">
+<div class="wk-gallery wk-gallery-wall clearfix polaroid ">
+        <a class="" href="<?php the_permalink() ?>" title=""><div><img src="<?=$post_info["wpcf-prev_img_1"][0]?>" width="200" height="150" alt="image1"><!-- <p class="title">Tony</p> --></div></a>
+        <a class="" href="<?php the_permalink() ?>" title=""><div><img src="<?=$post_info["wpcf-prev_img_2"][0]?>" width="200" height="150" alt="image1"><!-- <p class="title">Tony</p> --></div></a>
+
+</div>
+
+</td>
+<td>
+<h3><strong><?=the_time('j F Y');?></strong></h3>
+<h3><strong><?=get_the_time();?></strong></h3>
+</td>
+</tr>
+<tr>
+<td align="right" width="200">
+        <?php if ($post_info["wpcf-isset_photo"][0] == 1): ?>
+                <img class="alignnone  wp-image-775" alt="photo" src="http://foolsday.local/wp-content/uploads/2014/03/photo.png" width="60" height="60" />
+        <?php endif ?>
+
+        <?php if ($post_info["wpcf-isset_video"][0] == 1): ?>
+                <img class="alignnone  wp-image-776" alt="video" src="http://foolsday.local/wp-content/uploads/2014/03/video.png" width="60" height="60" />
+        <?php endif ?>
+</td>
+</tr>
+<tr>
+<td colspan="2"><em><?=$post_info["wpcf-title_text"][0]?>
+ <a href="" title="">Подробнее...</a>
+</em></td>
+</tr>
+</tbody>
+</table>
+
+
+
+          <div class="content clearfix">
+                <?php if (has_post_thumbnail()) : ?>
+                        <?php
+                        $width = get_option('thumbnail_size_w'); //get the width of the thumbnail setting
+                        $height = get_option('thumbnail_size_h'); //get the height of the thumbnail setting
+                        ?>
+                        <a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail(array($width, $height), array('class' => 'alignright post-thumbnail')); ?></a>
+                <?php endif; ?>
+                <?php the_content(''); ?>
+        </div>
+
+        <!-- <p class="links">
+                <a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php _e('Continue Reading', 'warp'); ?></a>
+                <?php comments_popup_link(__('No Comments', 'warp'), __('1 Comment', 'warp'), __('% Comments', 'warp'), "", ""); ?>
+        </p> -->
+
+        <h4><?php edit_post_link(__('Изменить данные события', 'warp'), '<p class="edit">','</p>'); ?></h4>
+
+</article>
+
 <?else:?>
 <?
 /**
@@ -118,5 +208,4 @@ else
         </p>
 
         <?php edit_post_link(__('Edit this post.', 'warp'), '<p class="edit">','</p>'); ?>
-
 <?endif;?>
